4043d2975b0e7d0509392093c0c620269c6b3548
JonSteinn <jonsteinn@gmail.com>
2017-06-13 20:04:07 +0000

    repeating decimal

---
+++ src/Repeating Decimal/C/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, d;
+    while (scanf("%d %d %d",&a,&b,&d) == 3)
+    {
+        int len = d + 3;
+        char buffer[len];
+        int i = 0;
+        buffer[len - 1] = '\0'; buffer[i++] = '0'; buffer[i++] = '.';
+        while (i < len - 1)
+        {
+            if (a < b)
+            {
+                a *= 10;
+                int n = a / b;
+                buffer[i++] = (char)('0' + n);
+                a -= n * b;
+                if (a == 0)
+                {
+                    while (i < len - 1)
+                    {
+                        buffer[i++] = '0';
+                    }
+                }
+            }
+            else
+            {
+                buffer[i++] = (char)('0' + 1);
+                a -= 7;
+            }
+        }
+        printf("%s\n",buffer);
+    }
+    return 0;
+}
