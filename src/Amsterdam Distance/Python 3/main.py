19797fa49e19b1f768a8d73c2e9974fc3cfe2cc2
JonSteinn <jonstienn@gmail.com>
2020-03-10 08:52:31 -0400

    1liner

--- src/Amsterdam Distance/Python 3/main.py
+++ src/Amsterdam Distance/Python 3/main.py
@@ -1,3 +1,6 @@
+# One liner edition:
+# print('%.6f' % (lambda ts,rs,r,ru,t1,r1,t2,r2: r2 if r1 == 0 else (r1 if r2 == 0 else (abs(r1-r2) * ru if t1 == t2 else min(abs(t1-t2)*(_r/rs)*r/ts * 3.1415926535897932384626433832795 + (abs(_r-r1) + abs(_r-r2)) * ru for _r in range(0,max(r1,r2)+1)))))(*(lambda x1,x2,x3,x4,x5,x6,x7: (x1,x2,x3,1/x2*x3,x4,x5,x6,x7))(*(lambda a,b,c: (int(a),int(b), float(c)))(*input().split()),*map(int, input().split()))))
+
 from math import pi
 
 class PolarSystem:
