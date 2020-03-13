2cf402f7b330d4d366a276a89de986b6c2ef168d
JonSteinn <jonstienn@gmail.com>
2020-03-13 10:25:32 -0400

    y combinator

--- src/Narrow Art Gallery/Python 3/main.py
+++ src/Narrow Art Gallery/Python 3/main.py
@@ -2,15 +2,10 @@
 # this and assignment within lambda is invald until 3.8
 
 # I cleaned my original code before 'onelinerfying' it but didn't 
-# saved it so I'll throw the original solution below, although 
+# save it so I'll throw the original solution below, although 
 # somewhat tedious (not using bitmasks)
 
-# TODO: Anonymous recursion to combine... 
-def optimal_blockage(mat, n, k, prev_col, recursive_memory):
-    return 0 if n < k or n == 0 or k==0 else (lambda x: recursive_memory.__setitem__((n,k,prev_col),x) or x)(min(sum(mat[i][j] for i in range(k)) for j in range(2) if prev_col & (1<<j) != 0) if n == k else min(min(optimal_blockage(mat, n-1, k-1, prev_col=j+1,recursive_memory=recursive_memory) + mat[n-1][j] for j in range(2) if prev_col & (1<<j) != 0),optimal_blockage(mat, n-1, k,3,recursive_memory=recursive_memory))) if (n,k,prev_col) not in recursive_memory else recursive_memory[(n,k,prev_col)]
-
-print((lambda a,b,c: sum(sum(a,[]))-optimal_blockage(a,b,c,3,{}))(*(lambda d, _: d)((lambda a, b: ([list(map(int, input().split())) for _ in range(a)], a, b))(*map(int, input().split())),input())))
-
+print((lambda a,b,c: sum(sum(a,[]))-(lambda f: (lambda x: x(x))(lambda y: f(lambda *args: y(y)(*args))))(lambda f: lambda mat,n,k,prev_col,recursive_memory: 0 if n < k or n == 0 or k==0 else (lambda x: recursive_memory.__setitem__((n,k,prev_col),x) or x)(min(sum(mat[i][j] for i in range(k)) for j in range(2) if prev_col & (1<<j) != 0) if n == k else min(min(f(mat, n-1, k-1, j+1,recursive_memory) + mat[n-1][j] for j in range(2) if prev_col & (1<<j) != 0),f(mat, n-1, k,3,recursive_memory))) if (n,k,prev_col) not in recursive_memory else recursive_memory[(n,k,prev_col)])(a,b,c,3,{}))(*(lambda d, _: d)((lambda w1, w2: ([list(map(int, input().split())) for _ in range(w1)], w1, w2))(*map(int, input().split())),input())))
 
 """
 recursive_memory = {}
