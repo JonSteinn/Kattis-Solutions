da0561040822d79e2acc23db43157f703f4f22a5
JonSteinn <jonsteinn@gmail.com>
2020-04-12 11:56:59 -0400

    +1

---
+++ src/Get Shorty/Python 3/main.py
@@ -0,0 +1,29 @@
+from heapq import heappop, heappush
+
+def djikstra(adj, src, dst):
+    open_set,closed_set = [(-1,src)],set()
+    while open_set:
+        f, v = heappop(open_set)
+        if v == dst:
+            return -f
+        closed_set.add(v)
+        for u,w in adj[v]:
+            if u not in closed_set:
+                heappush(open_set,(f*w, u))
+
+def main():
+    while True:
+        n,m = map(int,input().split())
+        if n + m == 0:
+            break
+        adj = [[] for _ in range(n)]
+        for _ in range(m):
+            *verties,w = input().split()
+            u,v = map(int,verties)
+            w = float(w)
+            adj[u].append((v,w))
+            adj[v].append((u,w))
+        print('%.4f' % djikstra(adj,0,n-1))
+
+if __name__ == "__main__":
+    main()
