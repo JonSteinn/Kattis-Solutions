31d8ae60fe6805612295a2c42d387b034727b58e
JonSteinn <jonstienn@gmail.com>
2020-04-12 07:03:10 -0400

    +

--- src/Reduced ID Numbers/Python 3/main.py
+++ src/Reduced ID Numbers/Python 3/main.py
@@ -1,15 +1,11 @@
 def main():
     n = int(input())
     ids = list(int(input()) for _ in range(n))
-    if n == 1:
-        print(1)
-    else:
-        i = 2
-        while True:
-            if len(set(x % i for x in ids)) == n:
-                print(i)
-                break
-            i += 1
+    while True:
+        if len(set(x % n for x in ids)) == len(ids):
+            print(n)
+            break
+        n += 1
 
 if __name__ == "__main__":
     main()
