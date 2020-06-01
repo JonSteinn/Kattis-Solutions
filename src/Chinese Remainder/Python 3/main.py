a8aecf17a65946f7de3c08cf49fe6bc42898be1a
JonSteinn <jonstienn@gmail.com>
2020-06-01 10:23:26 -0400

    +

---
+++ src/Chinese Remainder/Python 3/main.py
@@ -0,0 +1,12 @@
+def egcd(a, b):
+    return (lambda g,p,q: (g,q-(b//a)*p,p))(*egcd(b%a, a)) if a else (b,0,1)
+
+def crt(a,n,b,m):
+    return (lambda g,p,q: (lambda lcd: (((a * m * q + b * n * p) // g) % lcd, lcd))((n*m)//g))(*egcd(n,m))
+
+def main():
+    for _ in range(int(input())):
+        print(*crt(*map(int,input().split())))
+
+if __name__ == "__main__":
+    main()
