ec359b15276cfd9bb3d9c4fb5ff3fa5fc87dc908
Calcifer2002 <83289730+Calcifer2002@users.noreply.github.com>
2021-12-09 07:00:31 +0530

    Update imperialmeasurement.py

--- src/Imperial Measurement/Python 3/imperialmeasurement.py
+++ src/Imperial Measurement/Python 3/imperialmeasurement.py
@@ -1,8 +1,8 @@
-lis = [1, 1000, 12, 3, 22, 10, 8, 3]
-ind = {
+array = [1, 1000, 12, 3, 22, 10, 8, 3] #List storing values of the units
+units = {
     'thou': 0, 'th': 0,
     'inch': 1, 'in': 1,
-    'foot': 2, 'ft': 2,
+    'foot': 2, 'ft': 2,        #dictionary with assigned values to make it easier to iterate.
     'yard': 3, 'yd': 3,
     'chain': 4, 'ch': 4,
     'furlong': 5, 'fur': 5,
@@ -11,20 +11,20 @@ ind = {
 }
 
 s = input().split()
-val = int(s[0])
-f = ind[s[1]]
-t = ind[s[3]]
-if f == t:
-    print(val)
-elif f < t:
-    p = 1
-    while f != t:
-        f += 1
-        p /= lis[f]
-    print(p * val)
+val = int(s[0]) #isolating number
+u1 = units[s[1]]  #linking unit with dictionary
+u2 = units[s[3]]  #linking unit 2 with dictionary
+if u1 == u2:
+    print(val) #considering that both can be same units
+elif u1 < u2:
+    variable = 1
+    while u1 != u2:
+        u1 += 1
+        variable /= lis[f] #diving variable by the values repeatedly till f is same as t. This willl be the same value as dividing the the number repeatedly
+    print(variable * val)  #printing end product
 else:
-    p = 1
-    while f != t:
-        p *= lis[f]
-        f -= 1
-    print(val * p)
+    variable = 1
+    while u1 != u2:
+        variable *= array[u1] #multiplying p by the values repeatedly till f is same as t. This willl be the same value as multiplying the the number repeatedly
+        u1 -= 1
+    print(variable * val)
