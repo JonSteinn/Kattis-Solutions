97d9c8dbd0047f41dda52c5a008c9638a9efac32
JonSteinn <jonsteinn@gmail.com>
2022-02-16 20:55:59 +0000

    script update

--- scripts/add.py
+++ scripts/add.py
@@ -79,6 +79,8 @@ def create_readme(repo):
     pre = """# Kattis Solutions
 Solutions to the [Kattis archives](https://open.kattis.com/).
 
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/jonsteinn)
+
 ## Problems
 | Problem | Languages | :link: |
 | - | - | - |
