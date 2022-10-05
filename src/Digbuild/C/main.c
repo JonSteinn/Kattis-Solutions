75d7ea69851b2769fbaa6cd9f0dc1c426942e927
JonSteinn <jonsteinn@gmail.com>
2022-10-05 21:45:51 +0000

    fix format

--- src/Digbuild/C/main.c
+++ src/Digbuild/C/main.c
@@ -10,12 +10,12 @@ const ll MOD = 1000000007;
 ll f(ll n) {
   ll arr[4] = {1, 5, 17, 63};
   if (n < 4)
-	  return arr[n];
+      return arr[n];
   for (ll i = 3; i < n; i++) {
-	arr[0] = (MOD + (arr[3] << 1) + (arr[2] << 1) + (arr[2] << 2) - arr[0]) % MOD;
-	arr[0] ^= arr[1] ^= arr[0] ^= arr[1];
-	arr[1] ^= arr[2] ^= arr[1] ^= arr[2];
-	arr[2] ^= arr[3] ^= arr[2] ^= arr[3];
+    arr[0] = (MOD + (arr[3] << 1) + (arr[2] << 1) + (arr[2] << 2) - arr[0]) % MOD;
+    arr[0] ^= arr[1] ^= arr[0] ^= arr[1];
+    arr[1] ^= arr[2] ^= arr[1] ^= arr[2];
+    arr[2] ^= arr[3] ^= arr[2] ^= arr[3];
   }
   return arr[3];
 }
@@ -65,92 +65,92 @@ typedef long long ll;
 const ll MOD = 1000000007;
 
 typedef struct {
-	ll data[4][4];
+    ll data[4][4];
 } mat4;
 
 typedef struct {
-	ll data[4];
+    ll data[4];
 } vec4;
 
 /* Store result in rhs */
 void mat_prod(mat4 *lhs, mat4 *rhs) {
-	ll a, b, c, d;
-	for (int i = 0; i < 4; i++) {
-		a = lhs->data[0][0] * rhs->data[0][i] +
-			lhs->data[0][1] * rhs->data[1][i] +
-			lhs->data[0][2] * rhs->data[2][i] +
-			lhs->data[0][3] * rhs->data[3][i];
+    ll a, b, c, d;
+    for (int i = 0; i < 4; i++) {
+        a = lhs->data[0][0] * rhs->data[0][i] +
+            lhs->data[0][1] * rhs->data[1][i] +
+            lhs->data[0][2] * rhs->data[2][i] +
+            lhs->data[0][3] * rhs->data[3][i];
 
-		b = lhs->data[1][0] * rhs->data[0][i] +
-			lhs->data[1][1] * rhs->data[1][i] +
-			lhs->data[1][2] * rhs->data[2][i] +
-			lhs->data[1][3] * rhs->data[3][i];
+        b = lhs->data[1][0] * rhs->data[0][i] +
+            lhs->data[1][1] * rhs->data[1][i] +
+            lhs->data[1][2] * rhs->data[2][i] +
+            lhs->data[1][3] * rhs->data[3][i];
 
-		c = lhs->data[2][0] * rhs->data[0][i] +
-			lhs->data[2][1] * rhs->data[1][i] +
-			lhs->data[2][2] * rhs->data[2][i] +
-			lhs->data[2][3] * rhs->data[3][i];
+        c = lhs->data[2][0] * rhs->data[0][i] +
+            lhs->data[2][1] * rhs->data[1][i] +
+            lhs->data[2][2] * rhs->data[2][i] +
+            lhs->data[2][3] * rhs->data[3][i];
 
-		d = lhs->data[3][0] * rhs->data[0][i] +
-			lhs->data[3][1] * rhs->data[1][i] +
-			lhs->data[3][2] * rhs->data[2][i] +
-			lhs->data[3][3] * rhs->data[3][i];
+        d = lhs->data[3][0] * rhs->data[0][i] +
+            lhs->data[3][1] * rhs->data[1][i] +
+            lhs->data[3][2] * rhs->data[2][i] +
+            lhs->data[3][3] * rhs->data[3][i];
 
-		rhs->data[0][i] = (MOD + a) % MOD;
-		rhs->data[1][i] = (MOD + b) % MOD;
-		rhs->data[2][i] = (MOD + c) % MOD;
-		rhs->data[3][i] = (MOD + d) % MOD;
-	}
+        rhs->data[0][i] = (MOD + a) % MOD;
+        rhs->data[1][i] = (MOD + b) % MOD;
+        rhs->data[2][i] = (MOD + c) % MOD;
+        rhs->data[3][i] = (MOD + d) % MOD;
+    }
 }
 
 /* Store result in self */
 void mat_square(mat4 *mat) {
-	mat4 tmp;
-	memcpy(&tmp, mat, sizeof(mat4));
-	mat_prod(&tmp, mat);
+    mat4 tmp;
+    memcpy(&tmp, mat, sizeof(mat4));
+    mat_prod(&tmp, mat);
 }
 
 // Too lazy to change this but this requires identity mat as input
 // and us defining our matrix within the function...
 void mat_pow(mat4 *odds, ll n) {
-	mat4 mat = {.data = {{2LL, 6LL, 0LL, -1LL},
-						 {1LL, 0LL, 0LL, 0LL},
-						 {0LL, 1LL, 0LL, 0LL},
-						 {0LL, 0LL, 1LL, 0LL}}};
-	while (n > 1) {
-		if (n & 1) {
-			mat_prod(&mat, odds);
-			mat_square(&mat);
-			n = (n - 1) / 2;
-		} else {
-			mat_square(&mat);
-			n /= 2;
-		}
-	}
-	mat_prod(&mat, odds);
+    mat4 mat = {.data = {{2LL, 6LL, 0LL, -1LL},
+                         {1LL, 0LL, 0LL, 0LL},
+                         {0LL, 1LL, 0LL, 0LL},
+                         {0LL, 0LL, 1LL, 0LL}}};
+    while (n > 1) {
+        if (n & 1) {
+            mat_prod(&mat, odds);
+            mat_square(&mat);
+            n = (n - 1) / 2;
+        } else {
+            mat_square(&mat);
+            n /= 2;
+        }
+    }
+    mat_prod(&mat, odds);
 }
 
 int g(ll n, vec4 *vec) {
-	mat4 mat = {.data = {{1LL, 0LL, 0LL, 0LL},
-						 {0LL, 1LL, 0LL, 0LL},
-						 {0LL, 0LL, 1LL, 0LL},
-						 {0LL, 0LL, 0LL, 1LL}}};
-	mat_pow(&mat, n);
-	return (mat.data[0][0] * vec->data[0] + mat.data[0][1] * vec->data[1] +
-			mat.data[0][2] * vec->data[2] + mat.data[0][3] * vec->data[3]) %
-		MOD;
+    mat4 mat = {.data = {{1LL, 0LL, 0LL, 0LL},
+                         {0LL, 1LL, 0LL, 0LL},
+                         {0LL, 0LL, 1LL, 0LL},
+                         {0LL, 0LL, 0LL, 1LL}}};
+    mat_pow(&mat, n);
+    return (mat.data[0][0] * vec->data[0] + mat.data[0][1] * vec->data[1] +
+            mat.data[0][2] * vec->data[2] + mat.data[0][3] * vec->data[3]) %
+        MOD;
 }
 
 ll f(ll n) {
-	vec4 vec = {.data = {63LL, 17LL, 5LL, 1LL}};
-	if (n < 4)
-		return vec.data[3 - n];
-	return g(n - 3, &vec);
+    vec4 vec = {.data = {63LL, 17LL, 5LL, 1LL}};
+    if (n < 4)
+        return vec.data[3 - n];
+    return g(n - 3, &vec);
 }
 
 int main() {
-	ll n;
-	scanf("%lld", &n);
-	printf("%lld\n", f(n));
-	return 0;
+    ll n;
+    scanf("%lld", &n);
+    printf("%lld\n", f(n));
+    return 0;
 }
